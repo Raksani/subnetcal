64e7b8c86397f7a030dd54f6a7b4a2b52db832ab
Thaksina <thaksina.j@ku.th>
2018-11-29 11:43:02 +0700

    add ui and fx id

--- src/CalculatorUIController.java
+++ src/CalculatorUIController.java
@@ -1,3 +1,33 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
 public class CalculatorUIController {
+    @FXML
+    private RadioButton classA;
+    @FXML
+    private RadioButton classB;
+    @FXML
+    private RadioButton classC;
+    @FXML
+    private TextField ip_address;
+    @FXML
+    private ComboBox subnet_mask;
+    @FXML
+    private ComboBox mask_bits;
+    @FXML
+    private ComboBox subnet_bits;
+    @FXML
+    private ComboBox maximum_subnet;
+    @FXML
+    private ComboBox hosts_per_subnet;
+    @FXML
+    private TextField host_address_range;
+    @FXML
+    private TextField subnet_id;
+    @FXML
+    private TextField broadcast_address;
+
 
 }
